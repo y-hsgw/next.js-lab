0342241e23e3ec53f11624d0761d53cb70fb2623
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-04 21:14:00 +0900

    metaデータの追加

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard Page",
+};
+
 export default function Page() {
   return <h1>Hello, Next.js! This Page is Dashboard</h1>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Main Page",
+};
+
 export default function Home() {
   return <main>Main Page</main>;
 }

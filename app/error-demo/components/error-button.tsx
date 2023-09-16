bf321b8d1bab474c51ad2e9dec80a7ef5df1fc46
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-16 21:45:48 +0900

    ErrorButtonの実装

---
+++ app/error-demo/components/error-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@mui/material";
+import { memo, useCallback, useEffect, useState } from "react";
+import { ThrowError } from "./throw-error";
+
+export const ErrorButton: React.FC = memo(() => {
+  const [isError, setIsError] = useState(false);
+
+  const handleClick = useCallback(async () => {
+    setIsError(true);
+  }, []);
+
+  return (
+    <>
+      <Button variant="contained" color="error" onClick={handleClick}>
+        Execute Error
+      </Button>
+      {isError && <ThrowError />}
+    </>
+  );
+});
+
+ErrorButton.displayName = "ErrorButton";
---
+++ app/error-demo/components/throw-error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const ThrowError: React.FC = () => {
+  throw new Error("Error");
+};

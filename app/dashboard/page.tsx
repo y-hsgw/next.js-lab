bc4a5d080d6903f0430c430eca9d45bd1ce0c05a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-16 17:41:35 +0900

    ローディングの挙動確認

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,24 @@
 import { Metadata } from "next";
+import { Box, Container, Toolbar } from "@mui/material";
+import { sleep } from "../utils/sleep";
+import { Header } from "./components/header";
 
 export const metadata: Metadata = {
   title: "Dashboard Page",
 };
 
-export default function Page() {
-  return <h1>Hello, Next.js! This Page is Dashboard</h1>;
+export default async function Page() {
+  await sleep(5000);
+
+  return (
+    <Box display="flex">
+      <Header />
+      <Box component="main" flexGrow={1} height="100vh">
+        <Toolbar />
+        <Container sx={{ mt: 4 }}>
+          <h1>Hello, Next.js! This Page is Dashboard</h1>
+        </Container>
+      </Box>
+    </Box>
+  );
 }

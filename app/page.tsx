f20b76e75bede2856f36a0d689f81afb82ba2398
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-16 17:46:40 +0900

    Mainページにナビゲーション追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { Stack, Typography } from "@mui/material";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Main Page",
 };
 
 export default function Home() {
-  return <main>Main Page</main>;
+  return (
+    <main>
+      <Typography variant="h3" component="h1">
+        Main Page
+      </Typography>
+      <Stack component="nav">
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/request-cache">Request</Link>
+      </Stack>
+    </main>
+  );
 }

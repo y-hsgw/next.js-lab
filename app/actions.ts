91852dcf0426523cd35ee4e1f99fa8dc7bf1b8fa
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-09-18 14:22:12 +0900

    cookieのセキュア設定

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import { cookies } from "next/headers";
 
 export async function setCookie() {
-  cookies().set("testId", "1234");
+  cookies().set("testId", "1234", { httpOnly: true, secure: true });
 }
 
 export async function getCookie(name: string) {

80fd7ba4d6baaf016fb36aededc37f0a5960796b
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-11-01 22:45:52 +0900

    urqlのインストールと検証

--- app/urql-demo/page.tsx
+++ app/urql-demo/page.tsx
@@ -1,3 +1,27 @@
-export default function UrqlDemoPage() {
+import { registerUrql } from "@urql/next/rsc";
+import { cacheExchange, createClient, fetchExchange, gql } from "urql";
+
+const makeClient = () => {
+  return createClient({
+    url: "https://trygql.formidable.dev/graphql/basic-pokedex",
+    exchanges: [cacheExchange, fetchExchange],
+  });
+};
+
+const { getClient } = registerUrql(makeClient);
+
+const PokemonsQuery = gql`
+  query {
+    pokemons(limit: 10) {
+      id
+      name
+    }
+  }
+`;
+
+export default async function UrqlDemoPage() {
+  const { data } = await getClient().query(PokemonsQuery, {});
+  console.log(data);
+
   return <div>demo</div>;
 }
